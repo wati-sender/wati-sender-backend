b5e80644d9ea3cd26b3f26e271b56f8ebd04e96f
wati-sender <watisender@gmail.com>
2025-02-18 18:10:58 +0530

    Update account.js

--- routes/account.js
+++ routes/account.js
@@ -1,8 +1,15 @@
 import express from "express";
-import { addBulkAccounts, getAllAccounts } from "../controllers/account.js";
+import {
+  addBulkAccounts,
+  deleteMultipleAccounts,
+  deleteSingleAccount,
+  getAllAccounts,
+} from "../controllers/account.js";
 
 const AccountRoutes = express.Router();
 
 AccountRoutes.post("/add/bulk", addBulkAccounts);
 AccountRoutes.get("/all", getAllAccounts);
+AccountRoutes.post("/delete/single", deleteSingleAccount);
+AccountRoutes.post("/delete/multiple", deleteMultipleAccounts);
 export default AccountRoutes;
