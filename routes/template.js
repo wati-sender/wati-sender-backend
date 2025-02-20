557b6415eb1995e5e5461c8119e3c9fa31e6b799
wati-sender <watisender@gmail.com>
2025-02-20 13:52:26 +0530

    Update template.js

--- routes/template.js
+++ routes/template.js
@@ -3,6 +3,7 @@ import {
   createTemplate,
   getAllTemplates,
   createTemplateInAllAccounts,
+  getTemplateReviewStatus,
 } from "../controllers/template.js";
 
 const TemplateRoutes = express.Router();
@@ -10,4 +11,5 @@ const TemplateRoutes = express.Router();
 TemplateRoutes.post("/create", createTemplate);
 TemplateRoutes.post("/create/bulk", createTemplateInAllAccounts);
 TemplateRoutes.get("/:client_id/all", getAllTemplates);
+TemplateRoutes.post("/check/review/status",getTemplateReviewStatus)
 export default TemplateRoutes;
