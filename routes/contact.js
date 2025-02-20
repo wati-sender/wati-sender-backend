5102929c3be0cbd2e19327a26d43231b9b32897a
wati-sender <watisender@gmail.com>
2025-02-20 17:02:15 +0530

    Update contact.js

--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,17 @@
-import express from 'express';
-import { addBulkContacts, addSingleContact, getAllContacts } from '../controllers/contact.js';
+import express from "express";
+import {
+  addBulkContacts,
+  addSingleContact,
+  deleteMultipleContacts,
+  deleteSingleContact,
+  getAllContacts,
+} from "../controllers/contact.js";
 
 const ContactRoutes = express.Router();
 
-ContactRoutes.post("/add/single",addSingleContact);
-ContactRoutes.post("/add/bulk",addBulkContacts);
-ContactRoutes.get("/all",getAllContacts);
+ContactRoutes.post("/add/single", addSingleContact);
+ContactRoutes.post("/add/bulk", addBulkContacts);
+ContactRoutes.get("/all", getAllContacts);
+ContactRoutes.post("/delete/single", deleteSingleContact);
+ContactRoutes.post("/delete/multiple", deleteMultipleContacts);
 export default ContactRoutes;
