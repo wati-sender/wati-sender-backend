9712c9caf2ea00ba8367c89972195c5d65f9e547
wati-sender <watisender@gmail.com>
2025-02-24 22:34:03 +0530

    Update template.model.js

---
+++ models/template.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const templateSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      default: ""
+    },
+    accountsToAdd: { type: [mongoose.Schema.ObjectId], ref: "Accounts" },
+    successCount: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    failedCount: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Template", templateSchema);
