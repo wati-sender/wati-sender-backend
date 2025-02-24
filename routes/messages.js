2ee9903019fd4ea2f55d5d53dcc7b2763c75da8f
wati-sender <watisender@gmail.com>
2025-02-24 22:34:33 +0530

    Update messages.js

--- routes/messages.js
+++ routes/messages.js
@@ -1,7 +1,15 @@
-import express from 'express';
-import { sendBulkMessages } from '../controllers/message.js';
+import express from "express";
+import {
+  getAllCampaigns,
+  getCampaignByID,
+  getCampaignReportByAccount,
+  sendBulkMessages,
+} from "../controllers/message.js";
 
 const MessagesRoutes = express.Router();
 
-MessagesRoutes.post("/send/bulk",sendBulkMessages);
+MessagesRoutes.post("/send/bulk", sendBulkMessages);
+MessagesRoutes.get("/campaigns", getAllCampaigns);
+MessagesRoutes.get("/campaigns/:campaignId", getCampaignByID);
+MessagesRoutes.post("/campaign/report/account", getCampaignReportByAccount);
 export default MessagesRoutes;
