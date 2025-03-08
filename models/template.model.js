f818db102f930decef0f7bd5e5cfb0a96e0e9f45
wati-sender <watisender@gmail.com>
2025-03-08 14:47:33 +0530

    Update template.model.js

--- models/template.model.js
+++ models/template.model.js
@@ -5,7 +5,7 @@ const templateSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
-      default: ""
+      default: "",
     },
     accountsToAdd: { type: [mongoose.Schema.ObjectId], ref: "Accounts" },
     successCount: {
@@ -14,6 +14,21 @@ const templateSchema = new mongoose.Schema(
       default: 0,
     },
     failedCount: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    submittedForReview: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    reviewSentSuccess: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    reviewSentFail: {
       type: Number,
       required: true,
       default: 0,
