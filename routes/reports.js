f370e3a1b173adbb789efce54067e67def6bee34
wati-sender <watisender@gmail.com>
2025-03-17 21:35:39 +0530

    Update reports.js

--- routes/reports.js
+++ routes/reports.js
@@ -6,6 +6,7 @@ import {
   getAllAccountImportReports,
 } from "../controllers/accountReports.js";
 import { deleteMultipleTemplateCreateReport, deleteSingleBulkCreateReport, getAllBulkTemplateCreateReport, getTemplateBulkCreateReport } from "../controllers/templatesReports.js";
+import { getAllBulkTemplateCreateReportIDS } from "../controllers/idsControllers.js";
 
 const ReportsRoutes = express.Router();
 
@@ -17,6 +18,7 @@ ReportsRoutes.post("/account/delete/multiple", deleteMultipleAccountsReport);
 
 /* ------------------------- Template create reports ------------------------ */
 ReportsRoutes.get("/template/bulkcreate", getAllBulkTemplateCreateReport);
+ReportsRoutes.get("/template/bulkcreate/ids", getAllBulkTemplateCreateReportIDS);
 ReportsRoutes.get("/template/bulkcreate/:reportId", getTemplateBulkCreateReport);
 ReportsRoutes.post("/template/bulkcreate/delete/single", deleteSingleBulkCreateReport);
 ReportsRoutes.post("/template/bulkcreate/delete/multiple", deleteMultipleTemplateCreateReport);
