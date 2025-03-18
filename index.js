b10446ef2d6e8054e7cd43e3bde9c3f15e22c60e
wati-sender <watisender@gmail.com>
2025-03-18 18:03:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,14 +10,22 @@ import MessagesRoutes from "./routes/messages.js";
 import StatisticsRoutes from "./routes/statistics.js";
 import ReportsRoutes from "./routes/reports.js";
 import MediaRoutes from "./routes/mediaRoutes.js";
+import cron from "node-cron";
+import { refetchAccountStatus } from "./controllers/account.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: "50mb" }));
 
 connectToDB();
+
+// Cron job to fetch account status after every 2 hours
+cron.schedule("0 */2 * * *", () => {
+  console.log("Running task every 2 hours:", new Date().toLocaleTimeString());
+  refetchAccountStatus();
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -30,7 +38,7 @@ app.use("/api/accounts", AccountRoutes);
 app.use("/api/messages", MessagesRoutes);
 app.use("/api/statistics", StatisticsRoutes);
 app.use("/api/reports", ReportsRoutes);
-app.use("/api/media", MediaRoutes)
+app.use("/api/media", MediaRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
