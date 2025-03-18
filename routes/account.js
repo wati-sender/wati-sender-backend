ab8fd3e2536e3580ef84c5827e24dbee2ea99ab1
wati-sender <watisender@gmail.com>
2025-03-18 18:02:38 +0530

    Update account.js

--- routes/account.js
+++ routes/account.js
@@ -4,6 +4,7 @@ import {
   deleteMultipleAccounts,
   deleteSingleAccount,
   getAllAccounts,
+  refetchAccountStatus,
 } from "../controllers/account.js";
 import { getAccountIds } from "../controllers/idsControllers.js";
 
@@ -14,4 +15,5 @@ AccountRoutes.get("/all", getAllAccounts);
 AccountRoutes.post("/delete/single", deleteSingleAccount);
 AccountRoutes.post("/delete/multiple", deleteMultipleAccounts);
 AccountRoutes.get("/all/ids", getAccountIds);
+AccountRoutes.get("/status/refetch", refetchAccountStatus);
 export default AccountRoutes;
