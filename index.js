a69ae416b03c5874da0fdc41402d1b1b50b3ba3c
wati-sender <watisender@gmail.com>
2025-03-24 15:49:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 connectToDB();
 
