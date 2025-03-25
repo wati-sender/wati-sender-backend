264931802e516fe687fa26773eb7a117c3553a89
wati-sender <watisender@gmail.com>
2025-03-25 11:44:12 +0530

    Update messages.js

--- routes/messages.js
+++ routes/messages.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   getAllCampaigns,
   getCampaignByID,
+  getCampaignErrors,
   getCampaignReportByAccount,
   sendBulkMessages,
 } from "../controllers/message.js";
@@ -10,6 +11,7 @@ const MessagesRoutes = express.Router();
 
 MessagesRoutes.post("/send/bulk", sendBulkMessages);
 MessagesRoutes.get("/campaigns", getAllCampaigns);
+MessagesRoutes.post("/campaigns/errors", getCampaignErrors);
 MessagesRoutes.get("/campaigns/:campaignId", getCampaignByID);
 MessagesRoutes.post("/campaign/report/account", getCampaignReportByAccount);
 export default MessagesRoutes;
