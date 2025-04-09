a351c4a9c2739320d0f31253f6649f8956a69199
wati-sender <watisender@gmail.com>
2025-04-09 18:15:42 +0530

    Update statistics.js

--- routes/statistics.js
+++ routes/statistics.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { getStatistics } from "../controllers/statistics.js";
+import { protectRequest } from "../middlewares/auth.middleware.js";
 
 const StatisticsRoutes = express.Router();
 
-StatisticsRoutes.post("/get", getStatistics);
+StatisticsRoutes.post("/get", protectRequest, getStatistics);
 export default StatisticsRoutes;
