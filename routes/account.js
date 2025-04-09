7721b9c352b5ffc8c0e9550d95e73e15e835df7b
wati-sender <watisender@gmail.com>
2025-04-09 18:14:24 +0530

    Update account.js

--- routes/account.js
+++ routes/account.js
@@ -8,14 +8,15 @@ import {
   refetchAccountWallet,
 } from "../controllers/account.js";
 import { getAccountIds } from "../controllers/idsControllers.js";
+import { protectRequest } from "../middlewares/auth.middleware.js";
 
 const AccountRoutes = express.Router();
 
-AccountRoutes.post("/add/bulk", addBulkAccounts);
-AccountRoutes.get("/all", getAllAccounts);
-AccountRoutes.post("/delete/single", deleteSingleAccount);
-AccountRoutes.post("/delete/multiple", deleteMultipleAccounts);
-AccountRoutes.get("/all/ids", getAccountIds);
-AccountRoutes.get("/status/refetch", refetchAccountStatus);
-AccountRoutes.get("/wallet/refetch", refetchAccountWallet);
+AccountRoutes.post("/add/bulk", protectRequest, addBulkAccounts);
+AccountRoutes.get("/all", protectRequest, getAllAccounts);
+AccountRoutes.post("/delete/single", protectRequest, deleteSingleAccount);
+AccountRoutes.post("/delete/multiple", protectRequest, deleteMultipleAccounts);
+AccountRoutes.get("/all/ids", protectRequest, getAccountIds);
+AccountRoutes.get("/status/refetch", protectRequest, refetchAccountStatus);
+AccountRoutes.get("/wallet/refetch", protectRequest, refetchAccountWallet);
 export default AccountRoutes;
