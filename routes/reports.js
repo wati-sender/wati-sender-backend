1541949fd047b8fccbb5f458c062010f3ed4b1fe
wati-sender <watisender@gmail.com>
2025-04-09 18:15:26 +0530

    Update reports.js

--- routes/reports.js
+++ routes/reports.js
@@ -5,22 +5,64 @@ import {
   getAccountImportReports,
   getAllAccountImportReports,
 } from "../controllers/accountReports.js";
-import { deleteMultipleTemplateCreateReport, deleteSingleBulkCreateReport, getAllBulkTemplateCreateReport, getTemplateBulkCreateReport } from "../controllers/templatesReports.js";
+import {
+  deleteMultipleTemplateCreateReport,
+  deleteSingleBulkCreateReport,
+  getAllBulkTemplateCreateReport,
+  getTemplateBulkCreateReport,
+} from "../controllers/templatesReports.js";
 import { getAllBulkTemplateCreateReportIDS } from "../controllers/idsControllers.js";
+import { protectRequest } from "../middlewares/auth.middleware.js";
 
 const ReportsRoutes = express.Router();
 
 /* ------------------------- Account import reports ------------------------- */
-ReportsRoutes.get("/imports/accounts", getAllAccountImportReports);
-ReportsRoutes.get("/imports/accounts/:importId", getAccountImportReports);
-ReportsRoutes.post("/account/delete/single", deleteSingleAccountReport);
-ReportsRoutes.post("/account/delete/multiple", deleteMultipleAccountsReport);
+ReportsRoutes.get(
+  "/imports/accounts",
+  protectRequest,
+  getAllAccountImportReports
+);
+ReportsRoutes.get(
+  "/imports/accounts/:importId",
+  protectRequest,
+  getAccountImportReports
+);
+ReportsRoutes.post(
+  "/account/delete/single",
+  protectRequest,
+  deleteSingleAccountReport
+);
+ReportsRoutes.post(
+  "/account/delete/multiple",
+  protectRequest,
+  deleteMultipleAccountsReport
+);
 
 /* ------------------------- Template create reports ------------------------ */
-ReportsRoutes.get("/template/bulkcreate", getAllBulkTemplateCreateReport);
-ReportsRoutes.get("/template/bulkcreate/ids", getAllBulkTemplateCreateReportIDS);
-ReportsRoutes.get("/template/bulkcreate/:reportId", getTemplateBulkCreateReport);
-ReportsRoutes.post("/template/bulkcreate/delete/single", deleteSingleBulkCreateReport);
-ReportsRoutes.post("/template/bulkcreate/delete/multiple", deleteMultipleTemplateCreateReport);
+ReportsRoutes.get(
+  "/template/bulkcreate",
+  protectRequest,
+  getAllBulkTemplateCreateReport
+);
+ReportsRoutes.get(
+  "/template/bulkcreate/ids",
+  protectRequest,
+  getAllBulkTemplateCreateReportIDS
+);
+ReportsRoutes.get(
+  "/template/bulkcreate/:reportId",
+  protectRequest,
+  getTemplateBulkCreateReport
+);
+ReportsRoutes.post(
+  "/template/bulkcreate/delete/single",
+  protectRequest,
+  deleteSingleBulkCreateReport
+);
+ReportsRoutes.post(
+  "/template/bulkcreate/delete/multiple",
+  protectRequest,
+  deleteMultipleTemplateCreateReport
+);
 
 export default ReportsRoutes;
