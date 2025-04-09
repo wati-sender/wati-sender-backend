2c8a4227e21acbaaec69d14fff19eae9085c07bd
wati-sender <watisender@gmail.com>
2025-04-09 18:14:54 +0530

    Update mediaRoutes.js

--- routes/mediaRoutes.js
+++ routes/mediaRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { deleteMedia, getAllMedia, uploadMedia } from "../controllers/media.js";
 import multer from "multer";
+import { protectRequest } from "../middlewares/auth.middleware.js";
 
 const upload = multer();
 
 const MediaRoutes = express.Router();
 
-MediaRoutes.post("/upload", upload.single('file'), uploadMedia);
-MediaRoutes.get("/all", getAllMedia);
-MediaRoutes.post("/delete", deleteMedia);
+MediaRoutes.post("/upload", protectRequest, upload.single("file"), uploadMedia);
+MediaRoutes.get("/all", protectRequest, getAllMedia);
+MediaRoutes.post("/delete", protectRequest, deleteMedia);
 export default MediaRoutes;
