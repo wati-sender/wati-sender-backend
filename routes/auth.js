585d7eb54a62fb3fde46e07912e30c50e0d43dda
wati-sender <watisender@gmail.com>
2025-04-09 18:14:40 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { login, verifyUser } from "../controllers/auth.js";
+import { login, signUp, verifyUser } from "../controllers/auth.js";
 
 const AuthRoutes = express.Router();
 
+AuthRoutes.post("/signup", signUp);
 AuthRoutes.post("/login", login);
 AuthRoutes.get("/verify", verifyUser);
 export default AuthRoutes;
