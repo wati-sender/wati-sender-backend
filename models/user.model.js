b4dea9cd65310ef5550a23691ac3e3a61c355e98
wati-sender <watisender@gmail.com>
2025-04-09 18:13:56 +0530

    Create user.model.js

---
+++ models/user.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+// Required to submit template for preview
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", UserSchema);
